3862c14804fccd6d583ea4919f879bf9bf53f3ed
kirk-hadley <kirk@nunahealth.com>
2014-06-12 01:51:28 -0700

    project setup

---
+++ cleaners/starting.py
@@ -0,0 +1,50 @@
+__author__ = 'kirk'
+import pandas as pd
+import numpy as np
+from generate_company_config import *
+from util.convenience import slurpify, decrypt
+from util.phi_enforcer import is_aes_encrypted
+from PHICleaner import readers
+import os
+
+def decrypt_with_unique_id(company, path):
+        try:
+            config = generate_company_config(company)
+        except:
+           print "not a company"
+        if os.path.isfile(path):
+            df = slurpify(readers.encrypted_streaming_reader(path, config))
+        else:
+            print "file not found"
+        try:
+            for i in df.columns:
+                if i.startswith('hashed'):
+                    pass
+                else:
+                    if is_aes_encrypted(df[i]):
+                        df[i] = df[i].apply(lambda x: decrypt(x, config))
+                    else:
+                        pass
+
+            df['unique_identifier'] = df.ssn.map(str) + '_' + df.date_of_birth.map(str) + '_' + df.first_name
+
+            return df
+
+        except:
+            print "failed to decrypt"
+
+class EnrollmentCleaner:
+    def __init__(self, company, file):
+        self.company = company
+        self.file = file
+        self.dataframe = decrypt_with_unique_id(company, file)
+        #TODO create list of individuals with necessary static features from enrollment files
+
+
+
+class MedicalCleaner:
+    def __init__(self, company, file):
+        self.company = company
+        self.file = file
+        self.dataframe = decrypt_with_unique_id(company, file)
+
--- scratch.py
+++ scratch.py
@@ -2,11 +2,11 @@ __author__ = 'nuna'
 
 import pandas as pd
 
-claims_path = 'short_clean_medical_claims.csv'
+claims_path = 'data/short_clean_medical_claims.csv'
 claims = pd.read_csv(claims_path)
 
 
-enrollment_path = 'enrollment_unique_dependent_id.csv'
+enrollment_path = 'data/enrollment_unique_dependent_id.csv'
 enrollment = pd.read_csv(enrollment_path)
 
 
@@ -15,4 +15,3 @@ unique_claims = claims.drop_duplicates('employee_id', take_last=True)
 enrollment['unique_identifier'] = enrollment.ssn.map(str) + '_' + enrollment.date_of_birth.map(str) + '_' + enrollment.first_name
 
 claims['unique_identifier'] = claims.ssn.map(str) + '_' + claims.date_of_birth.map(str) + '_' + claims.first_name
-
