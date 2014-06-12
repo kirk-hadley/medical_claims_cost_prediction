c6e9a1da2de929b76d921047b775e78ebe43edaa
kirk-hadley <kirk@nunahealth.com>
2014-06-11 23:57:05 -0700

    added unique identifier

--- scratch.py
+++ scratch.py
@@ -9,22 +9,10 @@ claims = pd.read_csv(claims_path)
 enrollment_path = 'enrollment_unique_dependent_id.csv'
 enrollment = pd.read_csv(enrollment_path)
 
-pd.DataFrame(enrollment.city, claims.city)
-
-
-pd.concat([enrollment.city, claims.city], axis=1)
-
-
-claims.columns
-
-
-
-l = pd.DataFrame()
-
-for i in enrollment.employee_id:
-    if i in claims.employee_id.values:
-        l = l.append(enrollment[enrollment['employee_id'] == i])
-    else:
-        print 'no'
 
 unique_claims = claims.drop_duplicates('employee_id', take_last=True)
+
+enrollment['unique_identifier'] = enrollment.ssn.map(str) + '_' + enrollment.date_of_birth.map(str) + '_' + enrollment.first_name
+
+claims['unique_identifier'] = claims.ssn.map(str) + '_' + claims.date_of_birth.map(str) + '_' + claims.first_name
+
--- starting.py
+++ starting.py
@@ -6,13 +6,14 @@ from util.convenience import slurpify, decrypt
 from util.phi_enforcer import is_aes_encrypted
 from PHICleaner import readers
 import os
-def stream_to_df(company, file):
+
+def decrypt_with_unique_id(company, path):
         try:
             config = generate_company_config(company)
         except:
            print "not a company"
-        if os.path.isfile(file):
-            df = slurpify(readers.encrypted_streaming_reader(file, config))
+        if os.path.isfile(path):
+            df = slurpify(readers.encrypted_streaming_reader(path, config))
         else:
             print "file not found"
         try:
@@ -24,15 +25,29 @@ def stream_to_df(company, file):
                         df[i] = df[i].apply(lambda x: decrypt(x, config))
                     else:
                         pass
+
+            df['unique_identifier'] = df.ssn.map(str) + '_' + df.date_of_birth.map(str) + '_' + df.first_name
+
             return df
+
         except:
             print "failed to decrypt"
 
-class CleanerMedical:
+class EnrollmentCleaner:
     def __init__(self, company, file):
         self.company = company
         self.file = file
-        self.dataframe = stream_to_df(company, file)
+        self.dataframe = decrypt_with_unique_id(company, file)
+        #TODO create list of people with necessary static features from enrollment
+
+
+
+class MedicalCleaner:
+    def __init__(self, company, file):
+        self.company = company
+        self.file = file
+        self.dataframe = decrypt_with_unique_id(company, file)
+
 
 
 
